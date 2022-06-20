5d7522331ba0e72235f5783a62fd3bd461d3d76e
tab <2969117392@qq.com>
2022-06-20 15:18:06 +0800

    update guide

--- include/ini/parser.hpp
+++ include/ini/parser.hpp
@@ -12,24 +12,38 @@
 
 namespace tab{
 
+// Use smart pointer to avoid some mistakes. 
 using SectionPtr  = std::shared_ptr<Section>;
 using SectionsPtr = std::shared_ptr<std::map<std::string, SectionPtr>>;
 
+//
+// INIParser is a template class, which allows you to use different kinds of streams. 
+// The stream must have these methods: 
+// -- eof()
+// -- getline() 
+// -- swap()
+//
 template <typename _Stream>
 class INIParser{
 public:
     INIParser(_Stream& stream){
-        stream_.swap(stream);
+        stream_.swap(stream); // The constructor needs to use the offered stream.
         sections_.reset(new std::map<std::string, SectionPtr>);
         _parse();
-        stream_.swap(stream);
+        stream_.swap(stream); // Give the stream back. We do not need it anymore.
     }
 
     virtual SectionsPtr& sections(void){
         return sections_;
     }
 
-    
+    virtual void write_order(void){
+
+    }
+
+    virtual void write_disorder(void){
+
+    }
 protected:
     _Stream stream_;
     SectionsPtr sections_;
@@ -43,13 +57,15 @@ protected:
         while(!stream_.eof()){
             std::getline(stream_, buffer);
 
+            // The start of a line is character '#' means that this line is an annotation.
             if(buffer[0] == '#')
                 continue;
-
+            
+            // The start of a line is character '[' means that this line is a beginning of a section.
             if(buffer[0] == '['){
-                size_t end = buffer.find(']');
+                size_t end = buffer.find(']'); // Find the position of ']'
                 if(end != std::string::npos){
-                    previous.assign(buffer.begin()+1, buffer.begin()+end);
+                    previous.assign(buffer.begin() + 1, buffer.begin() + end); // Store current section.
                     auto temp = sections_->find(previous);
                     if(temp == sections_->end())
                         sections_->insert({previous, SectionPtr(new Section(previous))});
@@ -57,6 +73,7 @@ protected:
                 continue;
             }
 
+            // If a line is not an annotation or a section, it might be an assignment statement.
             if(buffer.size() != 0){
                 std::string key;
                 auto i = buffer.begin();
@@ -87,6 +104,7 @@ protected:
                     value.push_back(*i);
                 }
 
+                // Avoid finding the target everytime.
                 if (previous_ite == sections_->end() || previous_ite->first != previous)
                     previous_ite = sections_->find(previous);
                 previous_ite->second->at(key) = value;
@@ -99,8 +117,17 @@ protected:
 
 }; // class INIParser
 
+// class FileParser is an INIParser uses std::fstream.
 using FileParser = INIParser<std::fstream>;
-using StringParser = INIParser<std::stringstream>;
+
+// Class FileParser is an INIParser uses std::stringstream, 
+// which allows you to get the ini string.
+class StringParser : public INIParser<std::stringstream> {
+public:
+    std::string str(void){
+        return stream_.str();
+    }
+};
 
 } // namespace tab
 
